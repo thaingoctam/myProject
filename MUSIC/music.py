1811cf0fbd49f30370c55a2a87cd11006e237499
thaingoctam11cdt2@gmail.com <thaingoctam11cdt2@gmail.com>
2019-12-13 16:52:36 +0700

    music

---
+++ MUSIC/music.py
@@ -0,0 +1,294 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'music.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Player_Music(object):
+    def setupUi(self, Player_Music):
+        Player_Music.setObjectName("Player_Music")
+        Player_Music.resize(541, 357)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Player_Music.sizePolicy().hasHeightForWidth())
+        Player_Music.setSizePolicy(sizePolicy)
+        Player_Music.setAcceptDrops(False)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icon_music/headphone_app.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Player_Music.setWindowIcon(icon)
+        Player_Music.setToolTip("")
+        Player_Music.setAutoFillBackground(False)
+        Player_Music.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 0);\n"
+"")
+        Player_Music.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+        Player_Music.setTabShape(QtWidgets.QTabWidget.Rounded)
+        self.centralwidget = QtWidgets.QWidget(Player_Music)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 200, 541, 141))
+        self.frame.setStyleSheet("background-color:rgb(56, 103, 164);\n"
+"margin-bottom:0;\n"
+"padding-bottom:0;\n"
+"opacity:1;")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.horizontalSlider = QtWidgets.QSlider(self.frame)
+        self.horizontalSlider.setGeometry(QtCore.QRect(50, 90, 441, 41))
+        self.horizontalSlider.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.horizontalSlider.setStyleSheet("QSlider::groove:horizontal {\n"
+"    border: 1px solid #999999;\n"
+"    height: 3px; /* the groove expands to the size of the slider by default. by giving it a height, it has a fixed size */\n"
+"    background:white;\n"
+"\n"
+"}\n"
+"\n"
+"QSlider::handle:horizontal {\n"
+"    background: white;\n"
+"    border: 2px solid black;\n"
+"    width: 13px;\n"
+"    margin:-7px 0;\n"
+"    border-radius: 7px;\n"
+"}")
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setTickPosition(QtWidgets.QSlider.TicksAbove)
+        self.horizontalSlider.setTickInterval(10)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.label_01 = QtWidgets.QLabel(self.frame)
+        self.label_01.setEnabled(True)
+        self.label_01.setGeometry(QtCore.QRect(500, 100, 31, 21))
+        self.label_01.setAutoFillBackground(False)
+        self.label_01.setStyleSheet("background-color: rgb(56, 103, 164);\n"
+"color: rgb(255, 255, 255);")
+        self.label_01.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.label_01.setObjectName("label_01")
+        self.label_00 = QtWidgets.QLabel(self.frame)
+        self.label_00.setGeometry(QtCore.QRect(10, 100, 31, 21))
+        self.label_00.setStyleSheet("background-color: rgb(56, 103, 164);\n"
+"color: rgb(255, 255, 255);")
+        self.label_00.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_00.setObjectName("label_00")
+        self.play_pause_pushButton = QtWidgets.QPushButton(self.frame)
+        self.play_pause_pushButton.setGeometry(QtCore.QRect(220, 30, 51, 51))
+        self.play_pause_pushButton.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.play_pause_pushButton.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.play_pause_pushButton.setToolTip("Pause Song")
+        self.play_pause_pushButton.setStyleSheet(" QPushButton {\n"
+"\n"
+"boder:1px solid white;\n"
+"background-color: rgb(56, 103, 164);\n"
+"border-radius:23px; }\n"
+"\n"
+"QPushButton:hover {\n"
+"background-color: rgb(56, 103, 140);\n"
+"border:1px solid rgb(160, 160, 160);\n"
+" }\n"
+"\n"
+"\n"
+"\n"
+"\n"
+" ")
+        self.play_pause_pushButton.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("Icon_music/play_btt.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap("Icon_music/stop_btt.svg"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.play_pause_pushButton.setIcon(icon1)
+        self.play_pause_pushButton.setIconSize(QtCore.QSize(30, 30))
+        self.play_pause_pushButton.setCheckable(True)
+        self.play_pause_pushButton.setFlat(False)
+        self.play_pause_pushButton.setObjectName("play_pause_pushButton")
+        self.Next_pushButton = QtWidgets.QPushButton(self.frame)
+        self.Next_pushButton.setGeometry(QtCore.QRect(290, 30, 51, 51))
+        self.Next_pushButton.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.Next_pushButton.setStyleSheet(" QPushButton {\n"
+"\n"
+"boder:1px solid white;\n"
+"background-color: rgb(56, 103, 164);\n"
+"border-radius:23px; }\n"
+"\n"
+"QPushButton:hover {\n"
+"background-color: rgb(56, 103, 140);\n"
+"border:1px solid rgb(160, 160, 160);\n"
+" }")
+        self.Next_pushButton.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("Icon_music/next_btt.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.Next_pushButton.setIcon(icon2)
+        self.Next_pushButton.setIconSize(QtCore.QSize(30, 30))
+        self.Next_pushButton.setFlat(True)
+        self.Next_pushButton.setObjectName("Next_pushButton")
+        self.previous_pushButton = QtWidgets.QPushButton(self.frame)
+        self.previous_pushButton.setGeometry(QtCore.QRect(150, 30, 51, 51))
+        self.previous_pushButton.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.previous_pushButton.setStyleSheet(" QPushButton {\n"
+"\n"
+"boder:1px solid white;\n"
+"background-color: rgb(56, 103, 164);\n"
+"border-radius:23px; }\n"
+"\n"
+"QPushButton:hover {\n"
+"background-color: rgb(56, 103, 140);\n"
+"border:1px solid rgb(160, 160, 160);\n"
+" }")
+        self.previous_pushButton.setText("")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("Icon_music/previous_btt.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.previous_pushButton.setIcon(icon3)
+        self.previous_pushButton.setIconSize(QtCore.QSize(30, 30))
+        self.previous_pushButton.setFlat(True)
+        self.previous_pushButton.setObjectName("previous_pushButton")
+        self.Repeat_pushButton = QtWidgets.QPushButton(self.frame)
+        self.Repeat_pushButton.setGeometry(QtCore.QRect(70, 30, 51, 51))
+        self.Repeat_pushButton.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.Repeat_pushButton.setStyleSheet(" QPushButton {\n"
+"\n"
+"boder:1px solid white;\n"
+"background-color: rgb(56, 103, 164);\n"
+"border-radius:23px; }\n"
+"\n"
+"QPushButton:hover {\n"
+"background-color: rgb(56, 103, 140);\n"
+"border:1px solid rgb(160, 160, 160);\n"
+" }")
+        self.Repeat_pushButton.setText("")
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap("repeat_button.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.Repeat_pushButton.setIcon(icon4)
+        self.Repeat_pushButton.setIconSize(QtCore.QSize(30, 30))
+        self.Repeat_pushButton.setFlat(True)
+        self.Repeat_pushButton.setObjectName("Repeat_pushButton")
+        self.horizontalSlider_2 = QtWidgets.QSlider(self.frame)
+        self.horizontalSlider_2.setGeometry(QtCore.QRect(400, 40, 91, 31))
+        self.horizontalSlider_2.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.horizontalSlider_2.setRange(0,100)
+        self.horizontalSlider_2.setStyleSheet("QSlider::groove:horizontal {\n"
+"    border: 1px solid #999999;\n"
+"    height: 3px; /* the groove expands to the size of the slider by default. by giving it a height, it has a fixed size */\n"
+"    background:white;\n"
+"\n"
+"}\n"
+"\n"
+"QSlider::handle:horizontal {\n"
+"    background: white;\n"
+"    border: 2px solid black;\n"
+"    width: 13px;\n"
+"    margin:-7px 0;\n"
+"    border-radius: 7px;\n"
+"}")
+        self.horizontalSlider_2.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_2.setObjectName("horizontalSlider_2")
+        self.Voice_label = QtWidgets.QLabel(self.frame)
+        self.Voice_label.setEnabled(True)
+        self.Voice_label.setGeometry(QtCore.QRect(500, 40, 31, 31))
+        self.Voice_label.setAutoFillBackground(False)
+        self.Voice_label.setStyleSheet("background-color: rgb(56, 103, 164);\n"
+"color: rgb(255, 255, 255);")
+        self.Voice_label.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.Voice_label.setObjectName("Voice_label")
+        self.volume = QtWidgets.QPushButton(self.frame)
+        self.volume.setGeometry(QtCore.QRect(360, 40, 41, 31))
+        self.volume.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.volume.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.volume.setToolTip("Mute")
+        self.volume.setStyleSheet(" QPushButton {\n"
+"\n"
+"boder:none;\n"
+"background-color: rgb(56, 103, 164);\n"
+"border-radius:23px; }\n"
+"\n"
+"QPushButton:hover {\n"
+"background-color: rgb(56, 103, 140);\n"
+" }")
+        self.volume.setText("")
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap("Icon_music/volume_on.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon5.addPixmap(QtGui.QPixmap("Icon_music/volume_off.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.volume.setIcon(icon5)
+        self.volume.setIconSize(QtCore.QSize(25, 25))
+        self.volume.setCheckable(True)
+        self.volume.setFlat(False)
+        self.volume.setObjectName("volume")
+        self.Album = QtWidgets.QLabel(self.centralwidget)
+        self.Album.setGeometry(QtCore.QRect(0, 0, 221, 201))
+        self.Album.setStyleSheet("")
+        self.Album.setText("")
+        self.Album.setObjectName("Album")
+        self.Album.setScaledContents(True)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(216, 0, 331, 201))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        Player_Music.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(Player_Music)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 541, 21))
+        self.menubar.setAcceptDrops(True)
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setAutoFillBackground(False)
+        self.menuFile.setObjectName("menuFile")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        Player_Music.setMenuBar(self.menubar)
+        self.actionOpen_File = QtWidgets.QAction(Player_Music)
+        self.actionOpen_File.setShortcutContext(QtCore.Qt.WidgetShortcut)
+        self.actionOpen_File.setVisible(True)
+        self.actionOpen_File.setMenuRole(QtWidgets.QAction.NoRole)
+        self.actionOpen_File.setPriority(QtWidgets.QAction.HighPriority)
+        self.actionOpen_File.setObjectName("actionOpen_File")
+        self.actionOpen_Folder = QtWidgets.QAction(Player_Music)
+        self.actionOpen_Folder.setObjectName("actionOpen_Folder")
+        self.actionInfo_Song = QtWidgets.QAction(Player_Music)
+        self.actionInfo_Song.setObjectName("actionInfo_Song")
+        self.actionExit = QtWidgets.QAction(Player_Music)
+        self.actionExit.setObjectName("actionExit")
+        self.actionAbout_Author = QtWidgets.QAction(Player_Music)
+        self.actionAbout_Author.setObjectName("actionAbout_Author")
+        self.menuFile.addAction(self.actionOpen_File)
+        self.menuFile.addAction(self.actionOpen_Folder)
+        self.menuFile.addAction(self.actionInfo_Song)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionExit)
+        self.menuHelp.addAction(self.actionAbout_Author)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(Player_Music)
+        QtCore.QMetaObject.connectSlotsByName(Player_Music)
+
+    def retranslateUi(self, Player_Music):
+        _translate = QtCore.QCoreApplication.translate
+        Player_Music.setWindowTitle(_translate("Player_Music", "Player Music"))
+        self.label_01.setText(_translate("Player_Music", "0:00"))
+        self.label_00.setText(_translate("Player_Music", "0:00"))
+        self.Next_pushButton.setToolTip(_translate("Player_Music", "Next Song"))
+        self.previous_pushButton.setToolTip(_translate("Player_Music", "Previous Song"))
+        self.Repeat_pushButton.setToolTip(_translate("Player_Music", "Repeat All"))
+        self.Voice_label.setText(_translate("Player_Music", "0:00"))
+        self.menuFile.setTitle(_translate("Player_Music", "File"))
+        self.menuHelp.setTitle(_translate("Player_Music", "Help"))
+        self.actionOpen_File.setText(_translate("Player_Music", "Open File"))
+        self.actionOpen_File.setShortcut(_translate("Player_Music", "Ctrl+O"))
+        self.actionOpen_Folder.setText(_translate("Player_Music", "Open Folder"))
+        self.actionOpen_Folder.setShortcut(_translate("Player_Music", "Ctrl+D"))
+        self.actionInfo_Song.setText(_translate("Player_Music", "Info Song"))
+        self.actionInfo_Song.setShortcut(_translate("Player_Music", "Ctrl+I"))
+        self.actionExit.setText(_translate("Player_Music", "Exit "))
+        self.actionExit.setShortcut(_translate("Player_Music", "Ctrl+E"))
+        self.actionAbout_Author.setText(_translate("Player_Music", "About Author"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Player_Music = QtWidgets.QMainWindow()
+    ui = Ui_Player_Music()
+    ui.setupUi(Player_Music)
+    Player_Music.show()
+    sys.exit(app.exec_())
+
---
+++ MUSIC/music_app.py
@@ -0,0 +1,168 @@
+# _*_ coding:utf-8 _*_
+import sys
+from mutagen import File as MutaFile
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
+from os.path import expanduser
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from  PyQt5 import *
+from music import *
+class MyForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Player_Music()
+        self.ui.setupUi(self)
+        self.currentPlaylist = QMediaPlaylist()
+        self.player = QMediaPlayer()
+        self.ui.actionOpen_File.triggered.connect(self.openFile)
+        self.ui.actionOpen_Folder.triggered.connect(self.addFiles)
+        self.ui.play_pause_pushButton.clicked.connect(self.btnplaystate)
+        self.ui.previous_pushButton.clicked.connect(self.prevItemPlaylist)
+        self.ui.Next_pushButton.clicked.connect(self.nextItemPlaylist)
+        self.ui.volume.clicked.connect(self.volumestate)
+        self.ui.horizontalSlider.sliderMoved.connect(self.seekPosition)
+        self.ui.horizontalSlider_2.sliderMoved.connect(self.volume)
+        self.player.positionChanged.connect(self.qmp_positionChanged)
+        self.player.audioAvailableChanged.connect(self.changeAlbum)
+        self.player.durationChanged.connect(self.update_duration)
+        self.timer=QTimer(self)
+        self.show()
+
+    def openFile(self):
+        fileChoosen = QFileDialog.getOpenFileName(self, 'Open Music File', expanduser('~'), 'Audio (*.mp3 *.ogg *.wav)',
+                                                  '*.mp3 *.ogg *.wav')
+        if fileChoosen != None:
+            self.currentPlaylist.addMedia(QMediaContent(QUrl.fromLocalFile(fileChoosen[0])))
+
+    def folderOpen(self):
+        folderAc = QAction('Open Folder', self)
+        folderAc.setShortcut('Ctrl+D')
+        folderAc.setStatusTip('Open Folder (Will add all the files in the folder) ')
+        folderAc.triggered.connect(self.addFiles)
+        return folderAc
+
+
+    def addFiles(self):
+        folderChoosen = QFileDialog.getExistingDirectory(self, 'Open Music Folder', expanduser('~'))                      # HÀM trả về đường dẫn hiện có được người dùng chỉ đinh
+        if folderChoosen != None:
+            it = QDirIterator(folderChoosen)          # QDirIterator trả về 1 list các file name và đường dẫn của file đó ,Interator là hàm duyệt qua từng phần tử
+            it.next()                                 # Hàm trả về item hiện tại và di chuyển con trỏ dến item kế tiếp trong 1 trình vòng lặp
+            while it.hasNext():                       # Hàm trả về True nếu Iterator còn Item kế tiếp Item đang duyệt
+                if it.fileInfo().isDir() == False and it.filePath() != '.':            # QFileInfo::isDir()  trả về True nếu đối tượng trỏ tói đường dẫn, hoặc một liên kết. Ngược lại là False
+                    fInfo = it.fileInfo()             #QDirIterator::fileInfo() const hàm trả về thông tin filename,đường dẫn của file
+                    print(it.filePath(), fInfo.suffix())                                                                  # QFileInfo::suffix() const Hàm trả về phần mở rộng của File
+                    if fInfo.suffix() in ('mp3', 'ogg', 'wav'):
+                        print('added file ', fInfo.fileName())
+                        self.currentPlaylist.addMedia(QMediaContent(QUrl.fromLocalFile(it.filePath()))) #currentPlaylist::addMedia(const QMediaContent &content) thêm media vào danh sách phát hiện tại(playlist tạo mới nếu dùng Qmedialplaylist) . trả về True nếu thành công, và False nếu ngược lại
+                it.next()
+
+    def btnplaystate(self):
+        if self.ui.play_pause_pushButton.isChecked():
+             self.playHandler()
+        else:
+             self.pauseHandler()
+
+    def playHandler(self):
+        self.userAction = 1
+        if self.player.state() == QMediaPlayer.StoppedState:
+            if self.player.mediaStatus() == self.player.NoMedia:
+                # self.player.setMedia(QMediaContent(QUrl.fromLocalFile(self.currentFile)))
+                if self.currentPlaylist.mediaCount() == 0:  # mediaCount() Trả về số Item trong playlist
+                    self.statusBar().show()
+                    self.statusBar().showMessage('No song for play!')
+                    self.timer.start(7000)
+                    self.timer.timeout.connect(self.PlotUpdate)
+                if self.currentPlaylist.mediaCount() != 0:
+                    self.player.setPlaylist(self.currentPlaylist)
+            elif self.player.mediaStatus() == QMediaPlayer.LoadedMedia:
+                self.player.play()
+            elif self.player.mediaStatus() == QMediaPlayer.BufferedMedia:
+                self.player.play()
+        elif self.player.state() == QMediaPlayer.PlayingState:
+            pass
+        elif self.player.state() == QMediaPlayer.PausedState:
+            self.player.play()
+
+    def pauseHandler(self):
+        self.userAction = 2
+        self.player.pause()
+        self.timer.stop()
+
+    def volumestate(self):
+        if self.ui.volume.isChecked():
+            self.player.setMuted(True)
+            self.ui.Voice_label.setText('Muted')
+        else:
+            self.player.setMuted(False)
+            self.ui.Voice_label.setText('%d' % (int(self.ui.horizontalSlider_2.value())))
+
+    def PlotUpdate(self):
+        self.statusBar().clearMessage()
+        self.statusBar().hide()
+
+    def seekPosition(self, position):
+        sender = self.sender()
+        if isinstance(sender, QSlider):
+            if self.player.isSeekable():
+                self.player.setPosition(position)
+
+    def volume(self, position):
+        sender = self.sender()
+        if isinstance(sender, QSlider):
+            if self.ui.horizontalSlider.mouseReleaseEvent:
+               self.player.setVolume(position)
+               self.ui.Voice_label.setText('%d' % (int(position)))
+
+    def qmp_mediaStatusChanged(self):
+        if self.player.mediaStatus() == QMediaPlayer.LoadedMedia and self.userAction == 1:
+            durationT = self.player.duration()
+            self.ui.horizontalSlider.setRange(0, durationT)
+            self.ui.label_01.setText(
+                '%d:%02d' % (int(durationT / 60000), int((durationT / 1000) % 60)))
+            self.player.play()
+
+    def update_duration(self):
+
+            durationT = self.player.duration()
+            self.ui.horizontalSlider.setRange(0, durationT)
+            self.ui.label_01.setText(
+                '%d:%02d' % (int(durationT / 60000), int((durationT / 1000) % 60)))
+            self.player.play()
+
+
+    def qmp_positionChanged(self, position, senderType=False):
+        if senderType == False:
+            self.ui.horizontalSlider.setValue(position)
+            # update the text label
+        self.ui.label_00.setText('%d:%02d' % (int(position / 60000), int((position / 1000) % 60)))
+
+    def changeAlbum(self):
+        if self.player.isAudioAvailable():
+            pixmap = QtGui.QPixmap()
+            art_album=self.player.currentMedia().canonicalUrl().toString()
+            url_return= art_album.replace("file:///",'')
+            metadata = MutaFile(url_return)
+            for tag in metadata.tags.values():
+                if tag.FrameID == 'APIC':
+                   pixmap.loadFromData(tag.data)
+            self.ui.Album.setPixmap(pixmap)
+
+            self.ui.label_2.setText('Playing: %s' % self.player.metaData("Title"))
+
+
+
+
+    def prevItemPlaylist(self):
+        self.player.playlist().previous()
+
+    def nextItemPlaylist(self):
+        self.player.playlist().next()
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    w = MyForm()
+    w.show()
+    sys.exit(app.exec_())
